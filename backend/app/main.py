648846e48cee283ac5088b2d0acc087d29c038fd
Gk-Caldim <gokulcaldim@gmail.com>
2026-03-27 22:44:29 +0530

    deploy readyyy

--- backend/app/main.py
+++ backend/app/main.py
@@ -61,7 +61,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://dashboard-live-one-kappa.vercel.app/",
+    "https://dashboard-live-one-kappa.vercel.app",
 ]
 
 # Add FRONTEND_URL if it's set in environment variables
